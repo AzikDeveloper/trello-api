81a83214d258580ad384b94920b15effb51bc610
Azizbek Xushnazarov <a.khushnazarov@uic.grou>
2022-05-03 11:28:49 +0500

    ref: enable messages middleware

--- web/core/settings.py
+++ web/core/settings.py
@@ -57,7 +57,7 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    # 'django.contrib.messages.middleware.MessageMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'debug_toolbar.middleware.DebugToolbarMiddleware',
     # "querycount.middleware.QueryCountMiddleware",
