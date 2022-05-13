6e52c2cd8e6abdeee5938c8fd11a2f8a1e3329a2
Azizbek Xushnazarov <a.khushnazarov@uic.group>
2022-05-13 11:02:08 +0500

    static

--- web/core/settings.py
+++ web/core/settings.py
@@ -142,7 +142,7 @@ USE_TZ = True
 
 
 MEDIA_URL = '/media/'
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "staticfiles")]
